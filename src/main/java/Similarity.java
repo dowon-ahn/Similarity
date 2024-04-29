40ef40b66d49c54a97cb7b8b2c590349df2f397b
dowon-ahn <ahndw55@gmail.com>
2024-04-29 14:17:11 +0900

    length 메서드

--- src/main/java/Similarity.java
+++ src/main/java/Similarity.java
@@ -1,2 +1,13 @@
 public class Similarity {
+    public double calculateScoreByLength(String str1, String str2) {
+        int minLength = Math.min(str1.length(), str2.length());
+        int maxLength = Math.max(str1.length(), str2.length());
+
+        if(maxLength >= minLength * 2){
+            return 0;
+        }
+
+        double gap = maxLength - minLength;
+        return (1 - (gap / minLength)) * 60;
+    }
 }
